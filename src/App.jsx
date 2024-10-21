a8cc496d1a18fc963b097a2b89f514497d05d7b8
axwvvvvv <galiyevamirbek84@gmail.com>
2024-10-21 15:59:27 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <div className="container">
+          <div className="wrapper">
+          <div className="cards1">
+            <img src="https://cdn.shopify.com/s/files/1/0640/3846/9846/files/air-jordan-1-retro-high-off-white-chicago-the-ten-469946.png?v=1723553375" alt="" />
+            <h1>Nike X OFF-white</h1>
+            <p>The 10: air Jordan 1 off-white - Chicago</p>
+            <b>$999</b>
+            <button>Buy Now</button>
+           
+          </div>
+
+          <div className="cards2">
+            <img src="https://heycatan.myshopify.com/cdn/shop/products/aq0818_148_1_1000x.png?v=1563938446" alt="" />
+            <h1>Nike X OFF-white</h1>
+            <p>The 10: air Jordan 1 off-white - Chicago</p>
+            <b>$999</b>
+            <button>Buy Now</button>
+           
+          </div>
+          </div>
+
+
+         
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
